5caf3f25c1e117043df61883899437e0c4c7af31
Thomas Erlang <thomas@erlang.dk>
2018-05-18 20:59:03 +0200

    Auto import modules from tasks and commands

--- tbot/commands/__init__.py
+++ tbot/commands/__init__.py
@@ -1,3 +1,7 @@
-from . import user_stream_watchtime
-from . import stream_uptime
-from . import cmd_bot
+import pkgutil
+
+__all__ = []
+for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
+    __all__.append(module_name)
+    module = loader.find_module(module_name).load_module(module_name)
+    exec('%s = module' % module_name)
