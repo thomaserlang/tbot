8c39854f6981149ca8355af2cca5aec4b60bd7ae
Thomas Erlang <thomas@erlang.dk>
2018-07-22 15:40:51 +0200

    Removed test data

--- tbot/discord_bot/twitch_sync.py
+++ tbot/discord_bot/twitch_sync.py
@@ -122,7 +122,7 @@ async def discord_request(http_session, url, params=None, headers={}):
             return data
 
 async def get_subscribers(client, info):
-
+    '''
     return [
         {
             "_id": "e5e2ddc37e74aa9636625e8d2cc2e54648a30418",
@@ -141,6 +141,7 @@ async def get_subscribers(client, info):
             }
         },
     ]
+    '''
 
     headers = {
         'Authorization': 'OAuth {}'.format(info['twitch_token']),
