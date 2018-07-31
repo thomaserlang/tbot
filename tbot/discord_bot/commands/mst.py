7eae5ccaca59dc4cd44324ef4ec9e0316201c4b8
Thomas Erlang <thomas@erlang.dk>
2018-07-31 22:27:37 +0200

    Text

--- tbot/discord_bot/commands/mst.py
+++ tbot/discord_bot/commands/mst.py
@@ -4,13 +4,13 @@ from tbot.discord_bot import bot
 from random import randint
 from datetime import timedelta
 
-@bot.command(description='Sync subscriber roles from twitch. Must have `Manage roles` permission to use.')
+@bot.command()
 async def mst(ctx):
     try:
         seconds = randint(0, 86400)
         minutes, seconds = divmod(seconds, 60)
         hours, minutes = divmod(minutes, 60)
-        await ctx.send('Current time in MST: {}'.format(
+        await ctx.send('Current time in Michelle Summer Time: {}'.format(
             '{:02d}:{:02d}'.format(hours, minutes)
         ))
     except:
