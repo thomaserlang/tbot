84c2b1be2c24be7acf79aa4855d27bc168346013
Thomas Erlang <thomas@erlang.dk>
2018-08-05 12:46:21 +0200

    Fixed not showing sub chat alert for a gifted sub

--- tbot/twitch_bot/tasks/chat_alerts.py
+++ tbot/twitch_bot/tasks/chat_alerts.py
@@ -4,7 +4,7 @@ from tbot.twitch_bot import bot
 
 @bot.on('USERNOTICE')
 async def usernotice(**kwargs):
-    if kwargs['msg-id'] in ('sub', 'resub'):
+    if kwargs['msg-id'] in ('sub', 'resub', 'subgift'):
         '''
         {'channel': '#shroud', 'message': None, 'badges': 'subscriber/3,premium/1', 'color': '#1E90FF', 'display-name': 'Ai0li', 'emotes': '', 'id': '42e1c41f-23f5-4b9c-93f9-c7b7278400ff', 'login': 'ai0li', 'mod': '0', 'msg-id': 'resub', 'msg-param-months': '4', 'msg-param-sub-plan-name': 'Channel Subscription (meclipse)', 'msg-param-sub-plan': 'Prime', 'room-id': '37402112', 'subscriber': '1', 'system-msg': 'Ai0li just subscribed with Twitch Prime. Ai0li subscribed for 4 months in a row!', 'tmi-sent-ts': '1533233279635', 'turbo': '0', 'user-id': '44003032', 'user-type': ''}
         '''
