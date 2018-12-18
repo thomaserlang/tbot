23a0bf34585c18fd8cea225cb7a0d72ad4f80ad7
Thomas Erlang <thomas@erlang.dk>
2018-12-19 00:50:26 +0100

    Prevent bot spam on mysterygift

--- tbot/twitch_bot/tasks/chat_alerts.py
+++ tbot/twitch_bot/tasks/chat_alerts.py
@@ -3,9 +3,25 @@ from tbot import config, utils
 from tbot.twitch_bot import bot
 from .badge_log import badge_log
 
+sub_mystery_gift = {}
+
 @bot.on('USERNOTICE')
 async def usernotice(**kwargs):
-    if kwargs['msg-id'] in ('sub', 'resub', 'subgift'):
+
+    # Prevent spamming the chat when mystery subs are gifted
+    if kwargs['msg-id'] in ('submysterygift', 'anonsubmysterygift',):
+        gift_users = sub_mystery_gift.setdefault(kwargs['room-id'], {})
+        gift_users[kwargs['user-id']] = int(kwargs['msg-param-mass-gift-count'])
+    if kwargs['msg-id'] in ('subgift', 'subgift'):
+        if kwargs['room-id'] in sub_mystery_gift:
+            if kwargs['user-id'] in sub_mystery_gift[kwargs['room-id']]:
+                sub_mystery_gift[kwargs['room-id']][kwargs['user-id']] -= 1
+                if 0 == sub_mystery_gift[kwargs['room-id']][kwargs['user-id']]:
+                    del sub_mystery_gift[kwargs['room-id']][kwargs['user-id']]
+                else:
+                    return
+
+    if kwargs['msg-id'] in ('sub', 'resub', 'subgift', 'anonsubgift', 'giftpaidupgrade',):
         '''
         {'channel': '#shroud', 'message': None, 'badges': 'subscriber/3,premium/1', 'color': '#1E90FF', 'display-name': 'Ai0li', 'emotes': '', 'id': '42e1c41f-23f5-4b9c-93f9-c7b7278400ff', 'login': 'ai0li', 'mod': '0', 'msg-id': 'resub', 'msg-param-months': '4', 'msg-param-sub-plan-name': 'Channel Subscription (meclipse)', 'msg-param-sub-plan': 'Prime', 'room-id': '37402112', 'subscriber': '1', 'system-msg': 'Ai0li just subscribed with Twitch Prime. Ai0li subscribed for 4 months in a row!', 'tmi-sent-ts': '1533233279635', 'turbo': '0', 'user-id': '44003032', 'user-type': ''}
         '''
