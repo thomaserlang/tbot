65dace85f799097d61f1bcef488e4e710451331f
Thomas Erlang <thomas@erlang.dk>
2019-01-13 02:50:22 +0100

    0.34

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='tbot',
-    version='0.33',
+    version='0.34',
     author='Thomas Erlang',
     author_email='thomas@erlang.dk',
     url='',
