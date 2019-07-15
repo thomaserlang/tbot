d8b85d1cb762f68d5d7346a08a1b3556819a73c0
Thomas Erlang <thomas@erlang.dk>
2019-07-15 19:07:38 +0200

    Added faceit next level

--- tbot/twitch_bot/functions/faceit.py
+++ tbot/twitch_bot/functions/faceit.py
@@ -2,7 +2,8 @@ import logging
 from tbot.twitch_bot.var_filler import fills_vars, Send_error
 from tbot import config
 
-@fills_vars('faceit.username', 'faceit.elo', 'faceit.level')
+@fills_vars('faceit.username', 'faceit.elo', 'faceit.level', 
+    'faceit.next_level_points', 'faceit.next_level')
 async def faceit_elo(bot, channel, args, var_args, **kwargs):
     logging.info(var_args)
     if not var_args or \
@@ -16,6 +17,20 @@ async def faceit_elo(bot, channel, args, var_args, **kwargs):
     headers = {
         'Authorization': f'Bearer {config["faceit_apikey"]}',
     }
+
+    elos = (
+        (1, 'level 1'),
+        (801, 'level 2'),
+        (951, 'level 3'),
+        (1101, 'level 4'),
+        (1251, 'level 5'),
+        (1401, 'level 6'),
+        (1551, 'level 7'),
+        (1701, 'level 8'),
+        (1851, 'level 9'),
+        (2001, 'level 10'),
+    )
+
     async with bot.ahttp.get('https://open.faceit.com/data/v4/players', params=params, headers=headers) as r:
         if r.status == 404:
             raise Send_error('Unknow user on Faceit (usernames are case sensitive)')
@@ -25,8 +40,19 @@ async def faceit_elo(bot, channel, args, var_args, **kwargs):
         d = await r.json()
         if 'csgo' not in d['games']:
             raise Send_error('The user does not have CSGO in their Faceit profile')
+
+        next_level_points = 0
+        next_level = 'unknown'
+        for i, e in enumerate(elos):
+            if e[0] < d['games']['csgo']['faceit_elo']:
+                if i+1 < len(elos):
+                    next_level = elos[i+1][1]
+                    next_level_points = elos[i+1][0] - d['games']['csgo']['faceit_elo']
+
         return {
             'faceit.username': '',
             'faceit.elo': d['games']['csgo']['faceit_elo'],
             'faceit.level': d['games']['csgo']['skill_level_label'],
+            'faceit.next_level_points': next_level_points,
+            'faceit.next_level': next_level,
         }
