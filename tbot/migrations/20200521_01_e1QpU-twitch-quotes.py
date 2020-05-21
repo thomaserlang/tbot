b91d7ae52fa7b36a6d33c9018c00e5cfc1c94a47
Thomas Erlang <thomas@erlang.dk>
2020-05-21 15:44:10 +0200

    Fixed trigger

--- tbot/migrations/20200521_01_e1QpU-twitch-quotes.py
+++ tbot/migrations/20200521_01_e1QpU-twitch-quotes.py
@@ -29,7 +29,10 @@ steps = [
     '''),
 
     step('''
-        CREATE `twitch_quotes_BEFORE_INSERT` BEFORE INSERT ON `twitch_quotes` FOR EACH ROW
+        CREATE 
+        TRIGGER `twitch_quotes_BEFORE_INSERT` 
+        BEFORE INSERT ON `twitch_quotes` 
+        FOR EACH ROW
         BEGIN
             declare n int;
             set n = (select ifnull(max(number), 0)+1 from twitch_quotes where channel_id=new.channel_id);
