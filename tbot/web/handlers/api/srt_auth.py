28470fa6032582eae6c0370b20253d2af40b153f
Thomas Erlang <thomas@erlang.dk>
2021-12-28 13:29:30 +0100

    Fixed srt auth

--- tbot/web/handlers/api/srt_auth.py
+++ tbot/web/handlers/api/srt_auth.py
@@ -1,11 +1,12 @@
 from .base import Api_handler
 from tbot import config
+import logging
 
 class Handler(Api_handler):
 
     def post(self):
         if self.get_argument('on_event') == 'on_connect':
-            s = self.get_argument('srt_url', '').rsplit('/')
+            s = self.get_argument('srt_url', '').rsplit('/', 1)
             if len(s) == 2:
                 if s[1] in config['rtmp_keys']:
                     self.set_status(200)
