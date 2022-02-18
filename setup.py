b9d74b980bdc136f63c0a7a171cf8703dfc5fb9d
Thomas Erlang <thomas@erlang.dk>
2022-02-19 00:00:42 +0100

    Version 0.93

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='tbot',
-    version='0.92',
+    version='0.93',
     author='Thomas Erlang',
     author_email='thomas@erlang.dk',
     url='',
