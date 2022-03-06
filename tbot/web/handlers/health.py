cd875abc100adbef25f8e33e87f20462b92d1cd7
Thomas Erlang <thomas@erlang.dk>
2022-03-06 11:05:50 +0100

    More health checks

--- tbot/web/handlers/health.py
+++ tbot/web/handlers/health.py
@@ -1,12 +1,50 @@
+import asyncio
 from tornado.web import RequestHandler
 
+from tbot import utils
 class Handler(RequestHandler):
 
     async def get(self):
-        self.set_header('Content-Type', 'text/plain')
         if hasattr(self.application, 'shutting_down') and self.application.shutting_down == True:
+            self.set_header('Content-Type', 'text/plain')
             self.write('SHUTTING DOWN')
             self.set_status(503)
             return
-        self.set_status(200)
-        self.write('READY')
+        
+        result = await asyncio.gather(
+            self.db_check(),
+            self.redis_check(),
+        )
+        self.set_header('Content-Type', 'application/json')
+        self.write(utils.json_dumps(result))
+        if any([r['error'] for r in result]):
+            self.set_status(500)
+        else:
+            self.set_status(200)
+        self.finish()
+
+    async def db_check(self):
+        r = {
+            'error': False, 
+            'message': 'OK', 
+            'service': 'Database',
+        }
+        try:
+            await self.application.db.execute('SELECT 1')
+        except Exception as e:
+            r['error'] = True 
+            r['message'] = f'Error: {str(e)}'
+        return r
+    
+    async def redis_check(self):
+        r = {
+            'error': False, 
+            'message': 'OK', 
+            'service': 'Redis',
+        }
+        try:
+            await self.application.redis.ping()
+        except Exception as e:
+            r['error'] = True 
+            r['message'] = f'Error: {str(e)}'
+        return r
