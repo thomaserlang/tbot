ee8669a40c9201e02b18977d601e2c7423da938c
Thomas Erlang <thomas@erlang.dk>
2023-05-05 19:41:17 +0200

    Filter out non numbers for randint

--- tbot/twitch_bot/functions/randint.py
+++ tbot/twitch_bot/functions/randint.py
@@ -1,5 +1,5 @@
+import re
 from tbot.twitch_bot.var_filler import fills_vars, Send_error
-from tbot import utils
 from random import randint
 
 @fills_vars('randint',)
@@ -8,22 +8,21 @@ async def countdown(bot, var_args, args, **kwargs):
     to = 100
     try:
         if 'randint' in var_args:
-            if len(var_args['randint']) == 1:
-                to = int(var_args['randint'][0])
-            elif len(var_args['randint']) > 1:
-                from_ = int(var_args['randint'][0])
-                to = int(var_args['randint'][1])
+            num_args = [arg for arg in var_args['randint'] if re.match(r'^[0-9]+$', arg)]
+            if len(num_args) == 1:
+                to = int(num_args[0])
+            elif len(num_args) > 1:
+                from_ = int(num_args[0])
+                to = int(num_args[1])
+
+        num_args = [re.sub(r'^[^0-9]+', '', arg) for arg in args if re.match(r'^[0-9]+', arg)]
+        if len(num_args) == 1:
+            to = int(num_args[0])
+        elif len(num_args) > 1:
+            from_ = int(num_args[0])
+            to = int(num_args[1])
     except ValueError:
-        pass
-    
-    try:
-        if len(args) == 1:
-            to = int(args[0])
-        elif len(args) > 1:
-            from_ = int(args[0])
-            to = int(args[1])
-    except ValueError:
-        pass
+        raise Send_error('Arguments must be integers')
 
     if to <= from_:
         raise Send_error('First argument must be lower than the second')
