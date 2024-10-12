0e0b90627b2d511b2202d0204d540f4c9bd747fc
Thomas Erlang <thomas@erlang.dk>
2024-10-12 21:05:13 +0200

    Fix alias

--- tbot/twitch_bot/functions/alias.py
+++ tbot/twitch_bot/functions/alias.py
@@ -1,6 +1,7 @@
-from tbot.twitch_bot.var_filler import fills_vars, Send_break
-from tbot.twitch_bot.tasks.command import db_command
-import logging
+
+from tbot.twitch_bot.tasks.command import twitch_command
+from tbot.twitch_bot.var_filler import Send_break, fills_vars
+
 
 @fills_vars('alias')
 async def alias(var_args, **kwargs):
@@ -8,5 +9,5 @@ async def alias(var_args, **kwargs):
         if cmd in kwargs['cmd_history'] or len(kwargs['cmd_history']) > 9:
             continue
         kwargs['cmd_history'].append(cmd)
-        await db_command(cmd, '#'+kwargs['channel'], data=kwargs)
+        await twitch_command(cmd, '#'+kwargs['channel'], data=kwargs)
     raise Send_break()
