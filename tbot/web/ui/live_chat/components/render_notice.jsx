2f723c3c1f8a73792f09fbd769224b643ddeeb33
Thomas Erlang <thomas@erlang.dk>
2024-11-02 20:40:24 +0100

    Show provider in the notice

--- tbot/web/ui/live_chat/components/render_notice.jsx
+++ tbot/web/ui/live_chat/components/render_notice.jsx
@@ -1,6 +1,9 @@
+import { providerShort } from "./provider_short";
+
 export function RenderNotice({ msg }) {
   return (
     <div className="notice">
+      {providerShort(msg.provider)}
       <span className="text">{msg.message}</span>
     </div>
   );
