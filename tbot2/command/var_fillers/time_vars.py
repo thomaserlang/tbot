98dd0a71d93f57c302344e6d32688a0ac3e8ca19
Thomas Erlang <thomas@erlang.dk>
2025-01-04 20:13:39 +0100

    Time vars

---
+++ tbot2/command/var_fillers/time_vars.py
@@ -0,0 +1,26 @@
+import pytz
+
+from tbot2.common import ChatMessage, datetime_now
+
+from ..types import Command, TMessageVars
+from ..var_filler import fills_vars
+
+
+@fills_vars(
+    provider='all',
+    vars=('time',),
+)
+async def time(chat_message: ChatMessage, command: Command, vars: TMessageVars):
+    if not vars['time'].args:
+        raise ValueError(
+            'A timezone must be specified with time. Example {time Europe/Copenhagen}'
+        )
+    if vars['time'].args[0] not in pytz.all_timezones:
+        raise ValueError(
+            'Invalid timezone. Valid list: https://docs.botashell.com/vars/time'
+        )
+    dt = pytz.utc.localize(
+        datetime_now(),
+    ).astimezone(pytz.timezone(vars['time'].args[0]))
+
+    vars['time'].value = dt.strftime('%H:%M')
