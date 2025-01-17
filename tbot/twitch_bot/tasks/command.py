7f1090130007c87340b891625806edc5b6ee623f
Thomas Erlang <thomas@erlang.dk>
2025-01-17 20:07:56 +0100

    Shared chat

--- tbot/twitch_bot/tasks/command.py
+++ tbot/twitch_bot/tasks/command.py
@@ -38,6 +38,8 @@ def handle_command(nick, target, message, **kwargs):
     cmd = args.pop(0).lower().strip('!')
     if cmd == bot.user['login']:
         cmd = '__thebotname'
+    if kwargs['source-room-id'] and kwargs['source-room-id'] != kwargs['room-id']:
+        return
     if cmd in _cmd_lookup:
         f = _cmd_lookup[cmd]['func']
         bot.loop.create_task(
