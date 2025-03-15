d64f3b84fa7f202f73591381d9d8df8e933a8882
Thomas Erlang <thomas@erlang.dk>
2025-03-15 13:42:50 +0100

    Add cmd_var_fillers imports for channel gambling and quotes

---
+++ tbot2/channel_gambling/cmd_var_fillers/__init__.py
@@ -0,0 +1,2 @@
+from . import roulette_vars as roulette_vars
+from . import slots_vars as slots_vars
--- tbot2/command/var_fillers/__init__.py
+++ tbot2/command/var_fillers/__init__.py
@@ -1,3 +1,7 @@
+from tbot2.channel_gambling import cmd_var_fillers as cmd_var_fillers
+from tbot2.channel_quotes import cmd_var_fillers as cmd_var_fillers  # noqa: F811
+from tbot2.twitch import cmd_var_fillers as cmd_var_fillers  # noqa: F811
+
 from . import countdown_vars as countdown_vars
 from . import faceit_vars as faceit_vars
 from . import points_name_vars as points_name_vars
