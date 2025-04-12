5a29c89212f4af44baef9e76301d6cb1c443a5cb
Thomas Erlang <thomas@erlang.dk>
2025-04-12 19:12:04 +0200

    Add 'spotify' to Provider type and providerLabels

--- src/types/provider.type.ts
+++ src/types/provider.type.ts
@@ -1,8 +1,9 @@
-export type Provider = 'twitch' | 'youtube' | 'discord'
+export type Provider = 'twitch' | 'youtube' | 'discord' | 'spotify'
 
 export const providerLabels: { [key: string | Provider]: string } = {
     all: 'All',
     twitch: 'Twitch',
     youtube: 'YouTube',
     discord: 'Discord',
+    spotify: 'Spotify',
 } as const
