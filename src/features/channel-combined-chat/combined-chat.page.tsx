f576f4232123c70b8eb1bdc09e7773a35a581545
Thomas Erlang <thomas@erlang.dk>
2025-04-15 00:29:15 +0200

    Update document title in combined chat page

--- src/features/channel-combined-chat/combined-chat.page.tsx
+++ src/features/channel-combined-chat/combined-chat.page.tsx
@@ -1,9 +1,12 @@
 import { useCurrentChannel } from '@/features/channel/current-channel.provider'
+import { useDocumentTitle } from '@/utils/document-title'
 import { Container, Flex } from '@mantine/core'
 import { ChatlogsViewer } from './components/chat-viewer'
 
 export function Component() {
     const channel = useCurrentChannel()
+    useDocumentTitle(`Combined Chat - ${channel.display_name}`)
+
     return (
         <Container size="lg">
             <Flex direction="column" h="var(--tbot-content-height)">
