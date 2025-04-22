344a2e153a088d06de156e6299cecb1ee4afa598
Thomas Erlang <thomas@erlang.dk>
2025-04-22 18:35:10 +0200

    request_url_for fix still using port

--- tbot2/common/utils/request_url_for.py
+++ tbot2/common/utils/request_url_for.py
@@ -1,13 +1,14 @@
+from urllib.parse import urljoin
+
 from fastapi import Request
-from starlette.datastructures import URL
 
 from tbot2.config_settings import config
 
 
-def request_url_for(request: Request, name: str) -> URL:
-    url = request.url_for(name).replace(
-        hostname=config.base_url.host,
-        port=config.base_url.port,
-        scheme=config.base_url.scheme,
+def request_url_for(request: Request, name: str) -> str:
+    local_url = request.url_for(name)
+    url = urljoin(
+        str(config.base_url),
+        local_url.path,
     )
     return url
