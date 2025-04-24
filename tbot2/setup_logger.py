b58f9b39a87386604a33f480f587c039731c24cf
Thomas Erlang <thomas@erlang.dk>
2025-04-24 10:32:30 +0200

    Logger

--- tbot2/setup_logger.py
+++ tbot2/setup_logger.py
@@ -2,6 +2,9 @@ import sys
 
 import sentry_sdk
 from loguru import logger
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from sentry_sdk.integrations.loguru import LoggingLevels, LoguruIntegration
+from sentry_sdk.integrations.starlette import StarletteIntegration
 
 from tbot2.config_settings import config
 
@@ -10,13 +13,30 @@ logger.remove()
 sentry_sdk.init(
     dsn=config.sentry_dsn,
     send_default_pii=True,
+    integrations=[
+        LoguruIntegration(
+            level=LoggingLevels.INFO.value,
+            event_level=LoggingLevels.ERROR.value,
+            event_format='{message}',
+        ),
+        StarletteIntegration(),
+        FastApiIntegration(),
+    ],
 )
 format = (
-    '<green>{time:HH:mm:ss.SSS}</green> | '
+    '<green>{time}</green> | '
     '<level>{level}</level> | '
     '{message} | {extra} | '
     '<light-blue>{module}.{function}:{line}</light-blue>'
 )
+if config.debug:
+    format = (
+        '<green>{time:HH:mm:ss.SSS}</green> | '
+        '<level>{level}</level> | '
+        '{message} | {extra} | '
+        '<light-blue>{module}.{function}:{line}</light-blue>'
+    )
+
 logger.add(
     sys.stderr,
     colorize=True,
