8657971388e4fb13380e95b7f55582319cc1e666
Thomas Erlang <thomas@erlang.dk>
2025-05-13 20:05:47 +0200

    Fix badges not having a size when loading

--- ui/src/features/channel-combined-chat/components/badges.tsx
+++ ui/src/features/channel-combined-chat/components/badges.tsx
@@ -1,4 +1,5 @@
 import { ChannelId } from '@/features/channel/types/channel.types'
+import { Skeleton } from '@mantine/core'
 import { useGetTwitchBadges } from '../api/twitch-badges.api'
 import { ChatMessageBadge } from '../types/chat-message.type'
 import classes from './chat-message-line.module.css'
@@ -9,23 +10,25 @@ interface Props {
 }
 export function Badges({ channelId, badges }: Props) {
     const channelBadges = useGetTwitchBadges({ channelId })
-
-    if (channelBadges.isLoading) return null
-
     return (
         <>
             {badges.map((badge) => {
                 const badgeVersion =
                     channelBadges.data?.[`${badge.type}/${badge.id}`]
-                if (!badgeVersion) return null
+                if (!badgeVersion)
+                    return (
+                        <Skeleton
+                            component="span"
+                            key={`${badge.type}-${badge.id}`}
+                            className={classes['chat-badge']}
+                        />
+                    )
                 return (
                     <img
                         key={`${badge.type}-${badge.id}`}
-                        title={badgeVersion.title}
-                        src={badgeVersion.image_url_1x}
-                        alt={badgeVersion.id}
-                        width={18}
-                        height={18}
+                        title={badgeVersion?.title || badge.name}
+                        src={badgeVersion?.image_url_1x}
+                        alt={badge.type}
                         className={classes['chat-badge']}
                     />
                 )
