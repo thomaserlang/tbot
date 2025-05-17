2841811f1f78eaf86e6f02acdc284627cef8f627
Thomas Erlang <thomas@erlang.dk>
2025-05-17 16:56:05 +0200

    Only pad hour 1

--- ui/src/utils/date.ts
+++ ui/src/utils/date.ts
@@ -18,7 +18,7 @@ export function timeSinceStart(startDateTime: string | Date): string {
     const minutes = Math.floor((duration % 3600) / 60)
     const seconds = duration % 60
 
-    return `${hours.toString().padStart(2, '0')}:${minutes
+    return `${hours.toString().padStart(1, '0')}:${minutes
         .toString()
         .padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
